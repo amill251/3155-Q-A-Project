c1948e0262614cfe0c1d12ad6402ec7c7f026d5a
mrnealboy <nlemon@uncc.edu>
2021-11-02 13:22:48 -0400

    add a login page

--- flask/app.py
+++ flask/app.py
@@ -19,7 +19,7 @@ def page_routes(app):
     #Routes for Pages
     @app.route("/")
     def index():
-        return render_template("index.html")
+        return render_template("login.html")
 
 def api_routes(app):
     #Routes for Pages
