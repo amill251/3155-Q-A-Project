95c969882b89885e4055985ed9a3032730218f0e
amill251 <alexmiller4481@gmail.com>
2021-11-09 15:45:08 -0500

    Added More tables

--- flask/database/schema.sql
+++ flask/database/schema.sql
@@ -5,3 +5,50 @@ CREATE TABLE IF NOT EXISTS users  (
 	_username TEXT NOT NULL UNIQUE,
 	_password TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS replies  (
+	reply_id INTEGER PRIMARY KEY,
+	answer_id INTEGER,
+	question_id INTEGER,
+	user_id INTEGER NOT NULL,
+	contents TEXT,
+	date_created DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS answers  (
+	answer_id INTEGER PRIMARY KEY,
+	question_id INTEGER,
+	user_id INTEGER NOT NULL,
+	contents TEXT,
+	date_created DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS questions  (
+	question_id INTEGER PRIMARY KEY,
+	user_id INTEGER NOT NULL,
+	title TEXT
+	contents TEXT,
+	date_created DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS question_reactions  (
+	question_id INTEGER PRIMARY KEY,
+	user_id INTEGER NOT NULL,
+	reaction_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS reactions  (
+	reaction_id INTEGER PRIMARY KEY,
+	reaction_name INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS answer_vote  (
+	answer_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
+	vote_id INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vote  (
+	vote_id INTEGER NOT NULL,
+	vote_name TEXT NOT NULL
+);
