b6719ea7ce7cd8e3406b0be59fc525d2f89e4f37
amill251 <alexmiller4481@gmail.com>
2021-12-13 18:20:34 -0500

    Made final changes

--- flask_app/static/scripts/viewquestion.js
+++ flask_app/static/scripts/viewquestion.js
@@ -10,10 +10,10 @@ $(document).ready(() => {
 
 function loadQuestion() {
 
-    console.log(location.search);
+    //console.log(location.search);
 
     getQuestion((response) => {
-        console.log(response);
+        //console.log(response);
         questionId = response.data[0].question_id;
         userId = response.data[0].user_id;
 
@@ -22,12 +22,12 @@ function loadQuestion() {
                 $(`#question-${reactionItem} > button`).click(() => {
                     reactQuestion(questionId, reactionItem);
                 });
-                console.log(reactionItem);
-                $(`#question-${reactionItem} > .badge-success > p`).text(response.data[0].reactions[reactionItem]);
-                console.log(response.data[0].reactions[reactionItem]);
+                //console.log(reactionItem);
+                $(`#question-${reactionItem} > .badge-primary > p`).text(response.data[0].reactions[reactionItem]);
+                //console.log(response.data[0].reactions[reactionItem]);
             } else {
                 if (response.data[0].reactions[reactionItem] != null) {
-                    console.log(response.data[0].reactions[reactionItem]);
+                    //console.log(response.data[0].reactions[reactionItem]);
                     $(`#question-${response.data[0].reactions[reactionItem]} > button`).addClass('btn-reacted');
                 }
             }
@@ -36,10 +36,10 @@ function loadQuestion() {
         $('#report-button').click(() => {
             reportQuestion(questionId);
         });
-        console.log("------");
-        console.log(response.data[0]);
+        //console.log("------");
+        //console.log(response.data[0]);
         if (response.data[0].user_reported) {
-            console.log("Made it inside print statement");
+            //console.log("Made it inside print statement");
             $('#report-button').addClass("btn-reported");
             $('#report-button').text("Reported");
         }
@@ -59,8 +59,8 @@ function loadQuestion() {
             submitAnswer(questionId);
         });
         response.data.forEach(question => {
-            console.log(question)
-            console.log(question.contents);
+            //console.log(question)
+            //console.log(question.contents);
             $('.question-title').html(question.title);
             $('#question-body').html(question.contents);
             $('.q-timestamp').html(question.date_created);
@@ -128,18 +128,18 @@ function submitAnswer(questionId) {
 function loadAnswers() {
     console.log('Running Load Answers');
     getAnswers((response) => {
-        console.log('Get answers callback');
+        //console.log('Get answers callback');
         $('#answer-container').empty();
-        console.log(response.data);
+        //console.log(response.data);
         response.data.sort((a, b) => {
             return b.votes.total_votes - a.votes.total_votes;
         }).forEach(answerResponse => {
             $('#answer-container').append(createHTMLAnswerTemplate(answerResponse));
         });
 
-        console.log('Got to the end of the append');
+        //console.log('Got to the end of the append');
     }, location.search)
-    console.log('Finished Load Answers');
+    //console.log('Finished Load Answers');
 }
 
 function createHTMLAnswerTemplate(answerData) {
@@ -170,13 +170,14 @@ function createHTMLAnswerTemplate(answerData) {
 
     let answer = `
     <div class="card mb-3">
-    <button class="btn btn-danger ` + reportClass + `" onclick="reportAnswer(` + answerData.question_id + ', ' + answerData.answer_id + `)">` + reportText + ` <i
-    class="fas fa-flag"></i>` + `</button>
         <div class="card-header">
             <div class="row">
                 <div class="col-8">` + answerData.username + `</div>
                 <div class="col-4 text-end">` + datetime + `</div>
             </div>
+        </div>
+        <div class="p-2 d-flex justify-content-end">
+            <button class="btn btn-danger ` + reportClass + `" onclick="reportAnswer(` + answerData.question_id + ', ' + answerData.answer_id + `)">` + reportText + ` <i class="fas fa-flag"></i>` + `</button>
         </div>
         <div class="card-body d-flex">
             <div class="p-2"> 
@@ -196,34 +197,34 @@ function createHTMLAnswerTemplate(answerData) {
                 </div>
             </div>
         </div>
-        <div class="d-flex justify-content-center">
+        <div class="d-flex justify-content-center p-2">
         <span id="question-like">
-            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'like')" class="btn btn-secondary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'like') { return 'btn-reacted' }; return ''; })() + `"><i class="fas fa-heart"></i></button>
-            <span style="height: 20px;" class="badge badge-success mr-4">
+            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'like')" class="btn btn-primary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'like') { return 'btn-reacted' }; return ''; })() + `"><i class="fas fa-heart"></i></button>
+            <span style="height: 20px;" class="badge badge-primary mr-4">
             <p>${answerData.reactions['like']}</p>
             </span>
         </span>
         <span id="question-dislike">
-            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'dislike')" class="btn btn-secondary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'dislike') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-heart-broken"></i></button>
-            <span style="height: 20px;" class="badge badge-success mr-4">
+            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'dislike')" class="btn btn-primary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'dislike') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-heart-broken"></i></button>
+            <span style="height: 20px;" class="badge badge-primary mr-4">
             <p>${answerData.reactions['dislike']}</p>
             </span>
         </span>
         <span id="question-cry">
-            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'cry')" class="btn btn-secondary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'cry') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-sad-tear"></i></button>
-            <span style="height: 20px;" class="badge badge-success mr-4">
+            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'cry')" class="btn btn-primary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'cry') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-sad-tear"></i></button>
+            <span style="height: 20px;" class="badge badge-primary mr-4">
             <p>${answerData.reactions['cry']}</p>
             </span>
         </span>
         <span id="question-angry">
-            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'angry')" class="btn btn-secondary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'angry') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-angry"></i></button>
-            <span style="height: 20px;" class="badge badge-success mr-4">
+            <button onclick="reactAnswer( ` + answerData.answer_id + `,${answerData.question_id},'angry')" class="btn btn-primary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'angry') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-angry"></i></button>
+            <span style="height: 20px;" class="badge badge-primary mr-4">
             <p>${answerData.reactions['angry']}</p>
             </span>
         </span>
         <span id="question-laugh">
-            <button onclick="reactAnswer( ` + answerData.answer_id + `, ${answerData.question_id},'laugh')" class="btn btn-secondary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'laugh') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-laugh-beam"></i></button>
-            <span style="height: 20px;" class="badge badge-success mr-4">
+            <button onclick="reactAnswer( ` + answerData.answer_id + `, ${answerData.question_id},'laugh')" class="btn btn-primary mr-3 ` + (() => { if (answerData.reactions['user_reaction'] == 'laugh') { return 'btn-reacted' } return '' })() + `"><i class="fas fa-laugh-beam"></i></button>
+            <span style="height: 20px;" class="badge badge-primary mr-4">
             <p>${answerData.reactions['laugh']}</p>
             </span>
         </span>
@@ -238,8 +239,8 @@ function voteAnswer(answer_id, vote) {
         "answer_id": answer_id,
         "vote_name": vote
     }
-    console.log('----------------------------------------');
-    console.log(voteBody);
+    //console.log('----------------------------------------');
+    //console.log(voteBody);
     postVote(location.reload(), voteBody);
 }
 
